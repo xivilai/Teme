d245d7c085dea96036c91ca9deffecbfbff5a117
Slava Logos <logos.slava@gmail.com>
2020-07-14 12:37:37 +0300

    started doing the settings window

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.transparent = true;
+config.skipTaskbar = true;
+config.alwaysOnTop = true;
+
+module.exports = config;
--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, Menu, Tray} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, Menu, Tray} = require('electron');
+const path = require('path');
+let tray = null;
+let config = require('./config');
 
-app.commandLine.appendSwitch('remote-debugging-port', '8315');
-app.commandLine.appendSwitch('host-rules', 'MAP * 127.0.0.1');
+if (app !== undefined) {
+
+var debug = typeof v8debug === 'object' 
+  || /--debug|--inspect/.test(process.execArgv.join(' '));
+if (!debug) {
+  app.commandLine.appendSwitch('remote-debugging-port', '8315');
+  app.commandLine.appendSwitch('host-rules', 'MAP * 127.0.0.1');
+}
 
 function main() {
   createWindow();
-  let tray = null
-  tray = new Tray(path.join(__dirname, '/resources/win32/app-icon.png'));
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Options', type: 'normal', click: ()=> {  } },
-    { label: 'Close', type: 'normal', click: ()=> { app.quit(); } },
-  ])
-  tray.setToolTip('Teme');
-  tray.setContextMenu(contextMenu);
+  createTray();
+  
+  // let firstAppLaunch = process.argv.includes('--squirrel-firstrun');
+
+  // if (firstAppLaunch) {
+  //   seedSettings();
+  // }
 }
 
 function createWindow() {
   const window = new BrowserWindow({
     width: 175,
     height: 80,
-    skipTaskbar: true,
-    transparent: true,
+    skipTaskbar: config.skipTaskbar,
+    transparent: config.transparent,
     frame: false,
     icon: path.join(__dirname, '/resources/win32/app-icon.png'),
     webPreferences: {
@@ -31,22 +38,34 @@ function createWindow() {
   });
 
   window.loadFile('index.html');
+  window.setAlwaysOnTop(config.alwaysOnTop, 'normal');
 
-  window.setAlwaysOnTop(true, 'normal');
+  return window;
+}
+
+function createTray() {
+  tray = new Tray(path.join(__dirname, '/resources/win32/app-icon.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Options', type: 'normal', click: () => { createOptionsWindow(); } },
+    { label: 'Close', type: 'normal', click: () => { app.quit(); } },
+  ])
+  tray.setToolTip('Teme');
+  tray.setContextMenu(contextMenu);
 }
 
 function createOptionsWindow() {
   const optionsWindow = new BrowserWindow({
-    width: 175,
-    height: 80,
+    width: 320,
+    height: 300,
     skipTaskbar: true,
-    transparent: true,
-    frame: false,
-    icon: path.join(__dirname, '/resources/win32/app-icon.png'),
     webPreferences: {
       nodeIntegration: true
     }
   });
+
+  optionsWindow.loadFile('options.html');
+  optionsWindow.setAlwaysOnTop(true, 'normal');
+  optionsWindow.setMenu(null);
 }
 
 // This method will be called when Electron has finished
@@ -69,3 +88,4 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+}
